793900405f5b4b1fb49510c3ffe0b59963d3839a
Jkar <j.garcia97k@gmail.com>
2026-08-08 13:18:42 -0500

    Feat(servicioPedidos) se mueven los dtos al feature conrrespondiente se cra la interfas depedidos services si datos aun

---
+++ BackEnd/Pedidos.Api/Features/Pedidos/interfaces/IPedidosServices.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pedidos.Api.Features.Pedidos.interfaces
+{
+    public interface IPedidosServices
+    {
+        
+    }
+}
