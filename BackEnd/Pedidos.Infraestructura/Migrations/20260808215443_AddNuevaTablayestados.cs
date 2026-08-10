4efba31bf641cb52c2de4e8a828f87e51d2beee1
Jkar <j.garcia97k@gmail.com>
2026-08-09 22:24:51 -0500

    ersfref

---
+++ BackEnd/Pedidos.Infraestructura/Migrations/20260808215443_AddNuevaTablayestados.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pedidos.Infraestructura.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNuevaTablayestados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Estado",
+                table: "PedidoCola",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "PedidoCola");
+        }
+    }
+}
